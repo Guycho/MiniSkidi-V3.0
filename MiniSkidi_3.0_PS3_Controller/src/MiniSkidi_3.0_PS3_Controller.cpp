35ada71fad15717f3e0a95c665b20ee257d4c831
Guy <Guy@spearuav.com>
2024-04-15 13:29:19 +0300

    init logics

--- MiniSkidi_3.0_PS3_Controller/src/MiniSkidi_3.0_PS3_Controller.cpp
+++ MiniSkidi_3.0_PS3_Controller/src/MiniSkidi_3.0_PS3_Controller.cpp
@@ -1,10 +1,12 @@
 #include "inputs-outputs/outputs-handler.h"
 #include "inputs-outputs/inputs-handler.h"
+#include "logics/logics-handler.h"
 
 void setup()
 {
   inputs_handler_init();
   outputs_handler_init();
+  logics_handler_init();
 }
 
 void loop()
